3eabf6ad107fb3eef9c1cda1fd326e3a4c13db18
SilverLluvia <108458468+SilverLluvia@users.noreply.github.com>
2022-08-04 04:18:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,38 @@ const pages = [ main, section1, section2, section3, section4, section5 ];
 const beforeBtn = document.querySelectorAll('.beforeButton');
 const nextBtn = document.querySelectorAll('.nextButton');
 
+const btnColorList = [
+  { color: ['#FFDB94','#FFCDC2','#FFD1A7','#E0945C','#D7A5FF','#ACE4EC','#FF906C','#BCEA83','#6AD1C5'] },
+  { color: ['#34C700','#FFCDC2','#ACE4EC','#DAFAFF','#FFD894','#FF906C','#FFCDC2','#D7A5FF','#E0945C','#6AD1C5'] },
+  { color: ['#FF906C','#FFD1A7','#FFDB94','#6AD1C5','#DAFAFF','#D7A5FF','#ACE4EC','#FFCDC2','#34C700','#E0945C'] },
+  { color: ['#DAFAFF','#6AD1C5','#ACE4EC','#FF906C','#FFDB94','#FFD1A7','#D7A5FF','#E0945C','#FFCDC2'] },
+  { color: ['#FFDB94','#34C700','#6AD1C5','#FFCDC2','#FFD1A7','#ACE4EC','#E0945C','#DAFAFF','#A7A5FF'] }
+];
+
+addButtonColor();
+
+function addButtonColor() {
+  for (let i = 0 ; i < btnColorList.length ; i++) {
+    const btn = pages[i+1].querySelectorAll('.wordBox button');
+    for (let j = 0 ; j < btnColorList[i].color.length ; j++) {
+      // btn[j].addEventListener('mouseover', setColor(btn[j], btnColorList[i].color[j]));
+      btn[j].addEventListener('click', () => {
+        btn[j].style.backgroundColor = btnColorList[i].color[j];
+      });
+      btn[j].addEventListener('mouseover', () => {
+        btn[j].style.backgroundColor = btnColorList[i].color[j];
+      });
+      btn[j].addEventListener('mouseout', () => {
+        btn[j].style.backgroundColor = '#ffffff';
+      });
+    }
+  }
+}
+
+// function setColor(element, color) {
+//   element.style.backgroundColor = color;
+// }
+
 var page = 0;
 
 document.getElementById('startButton').addEventListener('click', start);
@@ -19,25 +51,31 @@ for (const button of nextBtn) {
 }
 
 function start() {
-  setTimeout(() => {    
+  setTimeout(() => {
     main.style.display = "none";
-    section1.style.display = "inline-block";
+    section1.style.display = "flex";
     page = 1;
   }, 100)
 }
 
 function goBeforePage() {
-  setTimeout(() => {    
-    pages[page].style.display = "none";
-    pages[page-1].style.display = "inline-block";
+  setTimeout(() => {
+    if (page == 1) {
+      pages[page].style.display = "none";
+      pages[page-1].style.display = "flex";
+    }
+    else {
+      pages[page].style.display = "none";
+      pages[page-1].style.display = "flex";
+    }
     page -= 1;
   }, 100)
 }
 
 function goNextPage() {
-  setTimeout(() => {    
+  setTimeout(() => {
     pages[page].style.display = "none";
-    pages[page+1].style.display = "inline-block";
+    pages[page+1].style.display = "flex";
     page += 1;
   }, 100)
 }
@@ -48,10 +86,15 @@ function changeSentence() {
     case 2:
     case 3:
     case 4:
-    case 5: 
+    case 5:
   }
 }
 
-function changeColorPalette() {
-  
-}
+// function changeColorPalette() {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+// }
+
